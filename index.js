38588904a04a6a6c8c7600f7b5d7559ef1a55e63
123abhishekgorasara-netizen <123abhishekgorasara@gmail.com>
2026-07-30 13:07:07 +0530

    let and var

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hey... I am Using JS");
+// console.log("Hey... I am Using JS");
+// let and var keyword
+var a=23;
+if(a>10){
+    var a=45;
+    console.log("Value of a inside block of if ="+a)
+}
+console.log("value of a outside block of if=" + a)
